37d8f838436c997e066194f5a483e6d40ed11228
srile <srile@MSI>
2024-09-11 16:28:07 +0530

    update

--- assign1/src/assign1/alphabet.java
+++ assign1/src/assign1/alphabet.java
@@ -1,12 +1,12 @@
 package assign1;
 
-public class alphabet {
+public class alphabet 
 	public static void main(String args[]) {
 		char ch='s';
 		if((ch>='a'&&ch<='z')||(ch>'A'&&ch<='Z'))
 		System.out.println("alphabet");
 		else
-			System.out.println("not an alphabet");
+			System.out.println("welcome");
 			
 		}
 		
