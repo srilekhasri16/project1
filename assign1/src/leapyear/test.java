b47fa585952110544b714d79e836ba8a597f2c30
srile <srile@MSI>
2024-09-11 17:26:21 +0530

    change

--- assign1/src/leapyear/test.java
+++ assign1/src/leapyear/test.java
@@ -4,9 +4,9 @@ public class test {
 	public static void main(String args[]){
 	int year=2024;
 	if(year%4==0&&year%100!=0)
-		System.out.println("Gievn year is leap year1");
+		System.out.println("Gievn year is leap year11");
 	else
-		System.out.println("Given year not a leap year1");
+		System.out.println("Given year not a leap year2");
 	}
 		
 
