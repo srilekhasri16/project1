09636a236e5daadf4bd35dd8df8d2a3bb7b00133
srilekhasri16 <srilusrilekha2003@gmail.com>
2024-09-11 16:27:10 +0530

    Update test.java
    updated

--- assign1/src/evenorodd/test.java
+++ assign1/src/evenorodd/test.java
@@ -4,9 +4,9 @@ public class test {
 	public static void main(String args[]) {
 		int num=11;
 		if(num%2==0)
-	         System.out.println("Given number is even ");
+	         System.out.println("Given number is odd ");
 	         else
-	        	 System.out.println("Given number is odd");
+	        	 System.out.println("Given number is even");
 			
 	}
 
